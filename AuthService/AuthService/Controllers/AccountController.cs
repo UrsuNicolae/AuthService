058de5861ebc3ca5614635461db8c7de23d4d39e
Ursu Nicolae <ursu.nicolae@isa.utm.md>
2022-09-29 16:32:27 +0300

    Add register method

---
+++ AuthService/AuthService/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using AuthService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+            var user = new AppUser()
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.ApplicationUsers.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    }
+}
